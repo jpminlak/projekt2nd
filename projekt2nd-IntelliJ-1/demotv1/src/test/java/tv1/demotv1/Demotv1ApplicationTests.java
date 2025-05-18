fd277b8c14bce5ba2dac0bf783244f95aa2c5f1b
jpminlak <hrk866@naver.com>
2025-05-18 14:13:30 +0900

    19강까지 재업로드

---
+++ projekt2nd-IntelliJ-1/demotv1/src/test/java/tv1/demotv1/Demotv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tv1.demotv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demotv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
