5894902e04e558ec18d914777f0390725575ecfd
jpminlak <hrk866@naver.com>
2025-05-18 14:22:58 +0900

    19강까지 다시 올림

---
+++ demotv1/src/main/java/tv1/demotv1/SpringConfig.java
@@ -0,0 +1,20 @@
+package tv1.demotv1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tv1.demotv1.repository.MemberRepository;
+import tv1.demotv1.repository.MemoryMemberRepository;
+import tv1.demotv1.service.MemberService;
+
+//@Configuration
+public class SpringConfig {
+
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+}
---
+++ demotv1/src/main/java/tv1/demotv1/domain/Member.java
@@ -0,0 +1,11 @@
+package tv1.demotv1.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    private Long id;
+    private String name;
+}
